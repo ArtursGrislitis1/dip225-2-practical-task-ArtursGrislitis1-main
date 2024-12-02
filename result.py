0c1c50b7f64e6deb00adba7b05806b98dc5a4484
Arturs Grislitis <Arturs.Grislitis@edu.rtu.lv>
2024-12-02 10:53:12 +0200

    02.12.2024

--- result.py
+++ result.py
@@ -1,14 +1,14 @@
 import pandas
 fails=pandas.read_excel("description.xlsx", sheet_name="LookupAREA")
 info_list = fails.values.tolist()
-get_info=input('Kāds reģions?')
+get_info=input()
 reg_id=0
 for row in info_list:
     if row[1]==get_info:
         reg_id=row[0]
         break
 if reg_id==0:
-    print('Reģions nav atrasts')
+    print(0)
     exit()
 CSV=pandas.read_csv('data.csv').values.tolist()
 geo_count=[]
